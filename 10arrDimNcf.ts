045c545d4c8fd923b0cecc25874cb8256a7f3f1d
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-04-23 02:34:10 -0300

    new

--- 10arrDimNcf.ts
+++ 10arrDimNcf.ts
@@ -5,31 +5,32 @@ v= 0, -7, -9, 1, 0, 0.
 La salida es: 1 positivos, 2 negativos y 3. */
 
 import * as r from "readline-sync";
-const dim: number = r.questionInt ("Dimension de N numeros: ");
-let numsArr: number [] = new Array (dim);
+
+const dim: number = r.questionInt("Dimension de N numeros: ");
+let numsArr: number[] = new Array(dim);
+
 for (let i: number = 0; i < numsArr.length; i++) {
-    numsArr[i] = r.questionInt("Ingrese un numero: ");
+  numsArr[i] = r.questionInt("Ingrese un numero: ");
 }
-function clasificarNumeros(numeros: number[]): {positivos: number, negativos: number, ceros: number} {
-    let positivos = 0;
-    let negativos = 0;
-    let ceros = 0;
-    
-    for (let i = 0; i < numeros.length; i++) {
-      if (numeros[i] > 0) {
-        positivos++;
-      } else if (numeros[i] < 0) {
-        negativos++;
-      } else {
-        ceros++;
-      }
+
+function countPosNegZero(arr: number[]): {pos: number, neg: number, zero: number} {
+  let pos: number = 0;
+  let neg: number = 0;
+  let zero: number = 0;
+
+  for (let i: number = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      pos++;
+    } else if (arr[i] < 0) {
+      neg++;
+    } else {
+      zero++;
     }
-    
-    return { positivos, negativos, ceros };
   }
-  
-  const numeros = [0, -7, -9, 1, 0, 0];
-  const resultado = clasificarNumeros(numeros);
-  
-  console.log(resultado);
-/*NO FUNCIONA*///
+
+  return {pos, neg, zero};
+}
+
+const count = countPosNegZero(numsArr);
+console.log(`positivos:${count.pos}, negativos:${count.neg} y ceros:${count.zero}`);
+
