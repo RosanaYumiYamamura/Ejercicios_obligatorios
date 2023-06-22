b4f3b869aa04e6804caf71780ce860d3a06456d5
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-06-22 11:25:09 -0300

    neww

--- 3calificaciones.ts
+++ 3calificaciones.ts
@@ -19,7 +19,7 @@ while(nombre !== ""){
     nota2 = rls.questionInt("Ingrese la nota de problemas: ");
     nota3 = rls.questionInt("Ingrese la nota de teorica: ");
 
-    if(nota1 <= 0 || nota1 > 11 || nota2 <= 0 || nota2 > 11 || nota3 <= 0 || nota3 > 11){
+    if(nota1 <= 0 || nota1 > 10 || nota2 <= 0 || nota2 > 10 || nota3 <= 0 || nota3 > 10){
         console.log("Error");
     }else{
         notaFinal = (nota1 * 0.1 + nota2 * 0.5 + nota3 * 0.4);
